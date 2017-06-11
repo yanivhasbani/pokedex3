1abd4b54a19cfb89d1beccebb98b4471afc13c40
Yaniv Hasbani <yaniv@insert.io>
2017-06-11 23:10:48 +0300

    First Commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Yaniv Hasbani on 6/11/17.
+//  Copyright © 2017 Yaniv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
+
